cbd354271a91f9e1fd5c249e74ae7c715fa98328
dmugjh <1092344021@qq.com>
2019-04-22 21:17:04 +0800

    opencv 的 测试

---
+++ firstOpenCV.py
@@ -0,0 +1,12 @@
+import cv2
+cap = cv2.VideoCapture(0)
+gjh = 1
+while(True):
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('test1',frame)
+    cv2.imshow('test2',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
